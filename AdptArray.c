3e12b98643cbca4630ac77a14d1a2d28e34cfec3
Lara Abu Hamad <laraabuhamad@gmail.com>
2023-03-20 20:24:39 +0200

    fixing memory leak.

--- AdptArray.c
+++ AdptArray.c
@@ -2,7 +2,6 @@
 // Created by Lara Abu Hamad on 15/03/2023.
 //
 
-#include <MacTypes.h>
 #include <stdlib.h>
 #include <string.h>
 #include "AdptArray.h"
@@ -19,6 +18,7 @@ typedef struct AdptArray_{
 PAdptArray CreateAdptArray(COPY_FUNC copyFunc, DEL_FUNC delFunc, PRINT_FUNC printFunc){
     PAdptArray pADT = malloc(sizeof(AdptArray_));
     if (pADT) {
+        pADT->pElement = NULL;
         pADT->size = 0;
         pADT->copyElement = copyFunc;
         pADT->delElement = delFunc;
@@ -39,7 +39,7 @@ void DeleteAdptArray(PAdptArray pADT){
 
 Result SetAdptArrayAt(PAdptArray pADT, int i, PElement pElement){
     PElement* newpElement;
-    if(NULL == pADT || NULL == pElement)
+    if(NULL == pADT || NULL == pElement || i < 0)
         return FAIL;
     if(i >= pADT->size) {
         newpElement = (PElement*) calloc(i+1, sizeof(PElement));
@@ -60,11 +60,8 @@ Result SetAdptArrayAt(PAdptArray pADT, int i, PElement pElement){
 
 PElement GetAdptArrayAt(PAdptArray pADT, int i){
     int newi = -1;
-    for(int j=0; j<pADT->size; j++){
-        if(NULL != pADT->pElement[j] && i == j){
-            newi = j;
-            break;
-        }
+    if(NULL != pADT->pElement[i]) {
+        newi = i;
     }
     return (newi>=0 && newi<pADT->size) ? pADT->copyElement(pADT->pElement[newi]) : NULL;
 }
